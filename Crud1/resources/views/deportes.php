1db405a87c042cc8a659e6b8d175043a1802c990
IvanNavarro <avanzacom_ivan@yahoo.es>
2020-01-22 03:47:19 +0000

    Create deportes.php

---
+++ Crud1/resources/views/deportes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>DEPORTES</title>
+</head>
+<body>
+	<h1>GENERAL</h1>
+	<a href="http://www.marca.com/">MARCA</a>
+	<br>
+	<a href="https://as.com/">AS</a>
+	
+</body>
+</html>
